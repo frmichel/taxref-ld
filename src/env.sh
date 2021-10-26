2a686020df5ca9fa604ce5d72e5fd769e6b2b92b
Franck Michel <frmichel@users.noreply.github.com>
2021-10-26 15:06:13 +0200

    Add env file

---
+++ src/env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: Franck MICHEL, University Cote d'Azur, CNRS, Inria
+# Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+
+export DATASET_VERSION=14.0
+export DATASET_DATE=2021-10-23
+
+# MongoDB database
+export DB=taxrefv14
--- src/taxref2mongo/run_all.sh
+++ src/taxref2mongo/run_all.sh
@@ -1,68 +1,68 @@
 #!/bin/bash
 # Author: F. Michel, UCA, CNRS, Inria
 
-TAXREFVER=14.0
-DBNAME=taxrefv14
+# Environment variables
+. ../env.sh
 
-mkdir -p dumps/${TAXREFVER}
+mkdir -p dumps/${DATASET_VERSION}
 
 log=run_all.log
 echo -n "" > $log
 
 
 DATATYPE=biblio
-./query_${DATATYPE}.sh           $TAXREFVER          &>> $log
-./import_${DATATYPE}.sh          $TAXREFVER $DBNAME  &>> $log
-tar cvfz dumps/${TAXREFVER}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${TAXREFVER}_*.json
-rm -f taxref_${DATATYPE}_${TAXREFVER}_*.json
+./query_${DATATYPE}.sh           $DATASET_VERSION      &>> $log
+./import_${DATATYPE}.sh          $DATASET_VERSION $DB  &>> $log
+tar cvfz dumps/${DATASET_VERSION}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${DATASET_VERSION}_*.json
+rm -f taxref_${DATATYPE}_${DATASET_VERSION}_*.json
 
 DATATYPE=interactions
-./query_${DATATYPE}.sh           $TAXREFVER          &>> $log
-./import_${DATATYPE}.sh          $TAXREFVER $DBNAME  &>> $log
-tar cvfz dumps/${TAXREFVER}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${TAXREFVER}_*.json
-rm -f taxref_${DATATYPE}_${TAXREFVER}_*.json
+./query_${DATATYPE}.sh           $DATASET_VERSION      &>> $log
+./import_${DATATYPE}.sh          $DATASET_VERSION $DB  &>> $log
+tar cvfz dumps/${DATASET_VERSION}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${DATASET_VERSION}_*.json
+rm -f taxref_${DATATYPE}_${DATASET_VERSION}_*.json
 
 DATATYPE=locations
-./query_${DATATYPE}.sh           $TAXREFVER          &>> $log
-./import_${DATATYPE}.sh          $TAXREFVER $DBNAME  &>> $log
-tar cvfz dumps/${TAXREFVER}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${TAXREFVER}_*.json
-rm -f taxref_${DATATYPE}_${TAXREFVER}_*.json
+./query_${DATATYPE}.sh           $DATASET_VERSION      &>> $log
+./import_${DATATYPE}.sh          $DATASET_VERSION $DB  &>> $log
+tar cvfz dumps/${DATASET_VERSION}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${DATASET_VERSION}_*.json
+rm -f taxref_${DATATYPE}_${DATASET_VERSION}_*.json
 
 DATATYPE=vernaculars
-./query_${DATATYPE}.sh           $TAXREFVER          &>> $log
-./import_${DATATYPE}.sh          $TAXREFVER $DBNAME  &>> $log
-tar cvfz dumps/${TAXREFVER}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${TAXREFVER}_*.json
-rm -f taxref_${DATATYPE}_${TAXREFVER}_*.json
+./query_${DATATYPE}.sh           $DATASET_VERSION      &>> $log
+./import_${DATATYPE}.sh          $DATASET_VERSION $DB  &>> $log
+tar cvfz dumps/${DATASET_VERSION}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${DATASET_VERSION}_*.json
+rm -f taxref_${DATATYPE}_${DATASET_VERSION}_*.json
 
 DATATYPE=media
-./query_${DATATYPE}.sh           $TAXREFVER          &>> $log
-./import_${DATATYPE}.sh          $TAXREFVER $DBNAME  &>> $log
-tar cvfz dumps/${TAXREFVER}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${TAXREFVER}_*.json
-rm -f taxref_${DATATYPE}_${TAXREFVER}_*.json
+./query_${DATATYPE}.sh           $DATASET_VERSION      &>> $log
+./import_${DATATYPE}.sh          $DATASET_VERSION $DB  &>> $log
+tar cvfz dumps/${DATASET_VERSION}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${DATASET_VERSION}_*.json
+rm -f taxref_${DATATYPE}_${DATASET_VERSION}_*.json
 
 
 DATATYPE=taxonomy
-./query_${DATATYPE}.sh           $TAXREFVER          &>> $log
-./import_${DATATYPE}.sh          $TAXREFVER $DBNAME  &>> $log
-tar cvfz dumps/${TAXREFVER}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${TAXREFVER}_*.json
-rm -f taxref_${DATATYPE}_${TAXREFVER}_*.json
+./query_${DATATYPE}.sh           $DATASET_VERSION      &>> $log
+./import_${DATATYPE}.sh          $DATASET_VERSION $DB  &>> $log
+tar cvfz dumps/${DATASET_VERSION}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${DATASET_VERSION}_*.json
+rm -f taxref_${DATATYPE}_${DATASET_VERSION}_*.json
 
 DATATYPE=externalIds
-./query_${DATATYPE}.sh           $TAXREFVER          &>> $log
-./import_${DATATYPE}.sh          $TAXREFVER $DBNAME  &>> $log
-tar cvfz dumps/${TAXREFVER}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${TAXREFVER}_*.json
-rm -f taxref_${DATATYPE}_${TAXREFVER}_*.json
+./query_${DATATYPE}.sh           $DATASET_VERSION      &>> $log
+./import_${DATATYPE}.sh          $DATASET_VERSION $DB  &>> $log
+tar cvfz dumps/${DATASET_VERSION}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${DATASET_VERSION}_*.json
+rm -f taxref_${DATATYPE}_${DATASET_VERSION}_*.json
 
 
 DATATYPE=status_types
-./query_${DATATYPE}.sh           $TAXREFVER          &>> $log
-./import_${DATATYPE}.sh          $TAXREFVER $DBNAME  &>> $log
-tar cvfz dumps/${TAXREFVER}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${TAXREFVER}_*.json
-rm -f taxref_${DATATYPE}_${TAXREFVER}_*.json
+./query_${DATATYPE}.sh           $DATASET_VERSION      &>> $log
+./import_${DATATYPE}.sh          $DATASET_VERSION $DB  &>> $log
+tar cvfz dumps/${DATASET_VERSION}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${DATASET_VERSION}_*.json
+rm -f taxref_${DATATYPE}_${DATASET_VERSION}_*.json
 
 DATATYPE=statuses
-./query_${DATATYPE}.sh           $TAXREFVER          &>> $log
-./import_${DATATYPE}.sh          $TAXREFVER $DBNAME  &>> $log
-tar cvfz dumps/${TAXREFVER}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${TAXREFVER}_*.json
-rm -f taxref_${DATATYPE}_${TAXREFVER}_*.json
+./query_${DATATYPE}.sh           $DATASET_VERSION      &>> $log
+./import_${DATATYPE}.sh          $DATASET_VERSION $DB  &>> $log
+tar cvfz dumps/${DATASET_VERSION}/taxref_${DATATYPE}.json.tgz  taxref_${DATATYPE}_${DATASET_VERSION}_*.json
+rm -f taxref_${DATATYPE}_${DATASET_VERSION}_*.json
 
